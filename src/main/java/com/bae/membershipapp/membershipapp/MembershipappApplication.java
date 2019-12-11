1e2a2120e4b5f267a319b4312e7c6c5d60879c9f
JacobsChris <chrisreesjacobs@gmail.com>
2019-12-11 11:34:26 +0000

    Initial commit

---
+++ src/main/java/com/bae/membershipapp/membershipapp/MembershipappApplication.java
@@ -0,0 +1,13 @@
+package com.bae.membershipapp.membershipapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembershipappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MembershipappApplication.class, args);
+    }
+
+}
