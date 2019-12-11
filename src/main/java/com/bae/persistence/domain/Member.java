3d866d47db49f13b79139476ccc468089ff761c9
JacobsChris <chrisreesjacobs@gmail.com>
2019-12-11 14:05:44 +0000

    Member Class Made

--- src/main/java/com/bae/persistence/domain/Member.java
+++ src/main/java/com/bae/persistence/domain/Member.java
@@ -1,4 +1,117 @@
 package com.bae.persistence.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private boolean paidMembership;
+    private boolean hasGloves;
+    private boolean hasShoes;
+    private boolean hasClothes;
+
+    public Member() {
+    }
+
+    public Member(String firstName, String lastName, boolean paidMembership, boolean hasGloves, boolean hasShoes, boolean hasClothes) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.paidMembership = paidMembership;
+        this.hasGloves = hasGloves;
+        this.hasShoes = hasShoes;
+        this.hasClothes = hasClothes;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isPaidMembership() {
+        return paidMembership;
+    }
+
+    public void setPaidMembership(boolean paidMembership) {
+        this.paidMembership = paidMembership;
+    }
+
+    public boolean isHasGloves() {
+        return hasGloves;
+    }
+
+    public void setHasGloves(boolean hasGloves) {
+        this.hasGloves = hasGloves;
+    }
+
+    public boolean isHasShoes() {
+        return hasShoes;
+    }
+
+    public void setHasShoes(boolean hasShoes) {
+        this.hasShoes = hasShoes;
+    }
+
+    public boolean isHasClothes() {
+        return hasClothes;
+    }
+
+    public void setHasClothes(boolean hasClothes) {
+        this.hasClothes = hasClothes;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", paidMembership=" + paidMembership +
+                ", hasGloves=" + hasGloves +
+                ", hasShoes=" + hasShoes +
+                ", hasClothes=" + hasClothes +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return getId() == member.getId() &&
+                getFirstName().equals(member.getFirstName()) &&
+                getLastName().equals(member.getLastName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getFirstName(), getLastName());
+    }
 }
