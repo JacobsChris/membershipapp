70c949c2222a179502019cfd5bbd76b3eb94b2e1
JacobsChris <chrisreesjacobs@gmail.com>
2019-12-11 15:03:22 +0000

    group class made hopefully

--- src/main/java/com/bae/persistence/domain/Group.java
+++ src/main/java/com/bae/persistence/domain/Group.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 public class Group {
@@ -14,10 +15,62 @@ public class Group {
     private String location;
     private int groupOfficers;
 
+    public Group() {
+    }
+
     public Group(String location, int groupOfficers) {
         this.location = location;
         this.groupOfficers = groupOfficers;
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getGroupOfficers() {
+        return groupOfficers;
+    }
+
+    public void setGroupOfficers(int groupOfficers) {
+        this.groupOfficers = groupOfficers;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Group group = (Group) o;
+        return getId() == group.getId() &&
+                getGroupOfficers() == group.getGroupOfficers() &&
+                getLocation().equals(group.getLocation());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getLocation(), getGroupOfficers());
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "id=" + id +
+                ", location='" + location + '\'' +
+                ", groupOfficers=" + groupOfficers +
+                '}';
+    }
+
 
 }
 
