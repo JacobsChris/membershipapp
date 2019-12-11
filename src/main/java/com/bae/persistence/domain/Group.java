67a3c31253d4a77629ca9182a5380255e2cd8185
JacobsChris <chrisreesjacobs@gmail.com>
2019-12-11 15:04:57 +0000

    group class made hopefully

--- src/main/java/com/bae/persistence/domain/Group.java
+++ src/main/java/com/bae/persistence/domain/Group.java
@@ -13,14 +13,12 @@ public class Group {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String location;
-    private int groupOfficers;
 
     public Group() {
     }
 
     public Group(String location, int groupOfficers) {
         this.location = location;
-        this.groupOfficers = groupOfficers;
     }
 
     public long getId() {
@@ -39,13 +37,6 @@ public class Group {
         this.location = location;
     }
 
-    public int getGroupOfficers() {
-        return groupOfficers;
-    }
-
-    public void setGroupOfficers(int groupOfficers) {
-        this.groupOfficers = groupOfficers;
-    }
 
     @Override
     public boolean equals(Object o) {
@@ -53,13 +44,12 @@ public class Group {
         if (o == null || getClass() != o.getClass()) return false;
         Group group = (Group) o;
         return getId() == group.getId() &&
-                getGroupOfficers() == group.getGroupOfficers() &&
                 getLocation().equals(group.getLocation());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getLocation(), getGroupOfficers());
+        return Objects.hash(getId(), getLocation());
     }
 
     @Override
@@ -67,11 +57,8 @@ public class Group {
         return "Group{" +
                 "id=" + id +
                 ", location='" + location + '\'' +
-                ", groupOfficers=" + groupOfficers +
                 '}';
     }
-
-
 }
 
 
