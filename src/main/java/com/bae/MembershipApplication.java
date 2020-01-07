60722ae5fc24e16fb6adfee739a0ec86970a489a
Chris <chrisreesjacobs@gmail.com>
2020-01-07 09:32:10 +0000

    made deployable through tomcat
    Signed-off-by: JacobsChris <chrisreesjacobs@gmail.com>

--- src/main/java/com/bae/MembershipApplication.java
+++ src/main/java/com/bae/MembershipApplication.java
@@ -2,12 +2,20 @@ package com.bae;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MembershipApplication {
+public class MembershipApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MembershipApplication.class, args);
+
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MembershipApplication.class);
     }
 
 }
