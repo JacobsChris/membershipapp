34f36b0723abcd1bfb5e3a22bd7858e8a6b3a267
Chris <chrisreesjacobs@gmail.com>
2020-01-07 09:32:10 +0000

    messing about with tabulator, unusual errors on webpage

--- src/main/resources/static/js/groupPage.js
+++ src/main/resources/static/js/groupPage.js
@@ -1,14 +1,5 @@
-//create Tabulator on DOM element with id "example-table"
-var table = new Tabulator("#groupTable", {
-    height: 205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
-    layout: "fitColumns", //fit columns to width of table (optional)
-    columns: [ //Define Table Columns
+var table = new Tabulator("#group-table", {
+    ajaxURL: "http://localhost:8080/gathering/getAll",
+    layout:"fitColumns",
 
-        {title: "Location", field: "location"},
-    ],
-    rowClick: function (e, row) { //trigger an alert message when the row is clicked
-        alert("Row " + row.getData().id + " Clicked!!!!");
-    },
 });
-
-table.setData("localhost:8080/gathering/getall");//,{},"getAll");
