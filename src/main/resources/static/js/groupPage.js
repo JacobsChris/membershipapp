d2f47514dbe414ce7a09d1790c4aaeaf4d5cd978
JacobsChris <chrisreesjacobs@gmail.com>
2020-01-07 16:10:27 +0000

    basic functionality of group and member tables
    Signed-off-by: JacobsChris <chrisreesjacobs@gmail.com>

--- src/main/resources/static/js/groupPage.js
+++ src/main/resources/static/js/groupPage.js
@@ -1,5 +1,64 @@
-var table = new Tabulator("#group-table", {
-    ajaxURL: "http://localhost:8080/gathering/getAll",
-    layout:"fitColumns",
-
+document.addEventListener("DOMContentLoaded", function () {
+    makeGroupTable();
 });
+
+function makeGroupTable() {
+    var table = new Tabulator("#groupTable", {
+        layout: "fitColumns",
+        ajaxURL: "http://localhost:8080/gathering/getAll",
+        rowClick: function (row) {
+            makeMemberTable()
+        },
+        columns: [
+            {title: "ID", field: "id"},
+            {title: "Location", field: "location"},
+
+        ],
+    });
+    removeElement("memberTable");
+    createElementWithID("div", "memberTable");
+}
+
+function makeMemberTable() {
+
+    var table = new Tabulator("#memberTable", {
+        layout: "fitColumns",
+        ajaxURL: "http://localhost:8080/member/getAll",
+        columns: [
+            {title: "First Name", field: "first_name"},
+            {title: "Second Name", field: "last_name"},
+            {title: "Membership", field: "paid_membership"},
+            {title: "Gloves", field: "has_gloves"},
+            {title: "Shoes", field: "has_shoes"},
+            {title: "Clothes", field: "has_clothes"},
+            {title: "Officer", field: "is_gathering_officer"},
+
+        ],
+    });
+    removeElement("groupTable");
+    createElementWithID("div", "groupTable");
+
+
+    let goBackButton = document.createElement("button");
+    goBackButton.innerHTML = "Go back to Groups";
+    let body = document.getElementById("groupTable");
+    body.appendChild(goBackButton);
+    goBackButton.addEventListener("click", function () {
+        makeGroupTable();
+    });
+
+}
+
+function removeElement(elementId) {
+    let element = document.getElementById(elementId);
+    element.parentNode.removeChild(element);
+}
+
+function createElementWithID(elementTag, elementId) {
+    let groupTable = document.createElement(elementTag);
+    groupTable.id = elementId;
+    let body = document.getElementsByTagName("body")[0];
+    body.appendChild(groupTable)
+
+}
+
