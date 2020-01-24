ce46ca009eda23341d7c591ecb7275461eca7cf7
Chris <chrisreesjacobs@gmail.com>
2020-01-24 13:48:52 +0000

    front end muckign about

--- src/main/resources/static/js/groupPage.js
+++ src/main/resources/static/js/groupPage.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     makeGroupTable();
 });
 
-let address = "3.11.111.10:9632/";
+let address = "https://3.11.111.10:9632/";
 
 function makeGroupTable() {
     document.getElementById("tableHeading").innerHTML = "Groups";
